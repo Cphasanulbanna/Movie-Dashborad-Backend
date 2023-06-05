5e03c20bde0db6c72f3abf1e17113ef6e33f7682
HASANUL BANNA C P <cphasanulbanna@gmail.com>
2023-06-06 02:51:07 +0530

    movie edit api compleetd

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -1,6 +1,9 @@
 //models
 const Movie = require("../models/movieModel");
 
+const path = require("path");
+const fs = require("fs");
+
 const addMovie = async (req, res) => {
     try {
         const { name, year, rating, leadActor, genre } = req.body;
@@ -9,6 +12,8 @@ const addMovie = async (req, res) => {
         if (isExists) {
             return res.status(400).json({ message: "movie with this title already exists" });
         }
+        const baseURL = `${req.protocol}://${req.get("host")}/images/`;
+        const posterPath = baseURL + poster;
 
         const newMovie = {
             name: name,
@@ -16,7 +21,7 @@ const addMovie = async (req, res) => {
             rating: rating,
             leadActor: leadActor,
             genre: genre,
-            poster: poster,
+            poster: posterPath,
         };
 
         const movie = await Movie.create(newMovie);
@@ -30,11 +35,56 @@ const addMovie = async (req, res) => {
 const fetchMovies = async (req, res) => {
     try {
         const movies = await Movie.find();
-        if()
+        if (!movies.length) {
+            return res.status(400).json({ message: "Movies not found!" });
+        }
+        res.status(200).json({ message: "Success", moviesList: movies });
     } catch (error) {
         console.log(error);
         res.status(400).json({ message: error.message });
     }
 };
 
-module.exports = { addMovie };
+const editMovie = async (req, res) => {
+    try {
+        const { name, year, rating, leadActor, genre, _id } = req.body;
+
+        const newPoster = req.file.filename;
+        const movie = await Movie.findById(_id);
+        if (!movie) {
+            return res.status(400).json({ message: "movie not found" });
+        }
+
+        if (newPoster) {
+            const baseURL = `${req.protocol}://${req.get("host")}/images/`;
+            const oldPoster = movie.poster;
+            const exactpath = oldPoster.replace(baseURL, "");
+            const imagePath = path.join(__dirname, "..", "public", "images", exactpath);
+
+            if (fs.existsSync(imagePath)) {
+                fs.unlinkSync(imagePath);
+            }
+
+            const newPosterPath = baseURL + newPoster;
+            movie.poster = newPosterPath;
+        }
+
+        if (name) {
+            movie.name = name;
+        }
+        if (year) {
+            movie.year = year;
+        }
+        movie.rating = rating;
+        movie.leadActor = leadActor;
+        movie.genre = genre;
+
+        const updatedMovie = await movie.save();
+        res.status(200).json({ message: "Movie updated successfully", movie: updatedMovie });
+    } catch (error) {
+        console.log(error);
+        res.status(400).json({ message: error.message });
+    }
+};
+
+module.exports = { addMovie, fetchMovies, editMovie };
--- routes/moviesRouter.js
+++ routes/moviesRouter.js
@@ -1,9 +1,11 @@
 //modules
 const express = require("express");
 const router = express.Router();
-const { addMovie } = require("../controllers/movieController");
+const { addMovie, fetchMovies, editMovie } = require("../controllers/movieController");
 const upload = require("../middleware/uploadImage");
 
+router.get("/", fetchMovies);
 router.post("/", upload.single("poster"), addMovie);
+router.put("/", upload.single("poster"), editMovie);
 
 module.exports = router;
