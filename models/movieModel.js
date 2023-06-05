a37a2e9d98356823ed3a2b9c45082ba577a56aa8
HASANUL BANNA C P <cphasanulbanna@gmail.com>
2023-06-06 00:53:41 +0530

    completed movieSchema

---
+++ models/movieModel.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+
+const movieSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            trim: true,
+            minLength: 2,
+            required: true,
+        },
+        year: {
+            type: Number,
+            required: true,
+            min: 1,
+        },
+        poster: {
+            type: String,
+            required: true,
+        },
+        rating: {
+            type: Number,
+            min: 1,
+            max: 1,
+        },
+        leadActor: {
+            type: String,
+        },
+        genre: [
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "Genres",
+            },
+        ],
+    },
+    { timestamps: true }
+);
+
+module.exports = mongoose.model("Movie", movieSchema);
