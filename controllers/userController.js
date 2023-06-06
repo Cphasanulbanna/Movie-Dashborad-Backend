3f8dadf96eb42ddc6bf23a599ff5831969875b3b
HASANUL BANNA C P <cphasanulbanna@gmail.com>
2023-06-07 00:31:20 +0530

    login api created

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,7 +2,8 @@
 const User = require("../models/userModel");
 
 //functions
-const { generatePasswordHash } = require("../utils/bcrypt");
+const { generatePasswordHash, comparePassword } = require("../utils/bcrypt");
+const { generateAccessToken } = require("../utils/jwt");
 
 const signup = async (req, res) => {
     try {
@@ -27,6 +28,34 @@ const signup = async (req, res) => {
         });
 
         return res.status(201).json({ message: "Account created successfully" });
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+};
+
+const login = async (req, res) => {
+    try {
+        const { username, password } = req.body;
+        if (!username || !password) {
+            return res.status(400).json({ message: "Username and Password is required" });
+        }
+        const user = await User.findOne({ username: username });
+        if (!user) {
+            return res.status(401).json({ message: "User not found" });
+        }
+
+        const validPassword = await comparePassword(password, user.password);
+        if (!validPassword) {
+            return res.status(404).json({ message: "Invalid password" });
+        }
+
+        const accessToken = generateAccessToken(user._id);
+        res.status(200).json({
+            message: "Login success",
+            _id: user.id,
+            username: user.username,
+            access_token: accessToken,
+        });
     } catch (error) {
         res.status(400).json({ message: error.message });
     }
--- utils/bcrypt.js
+++ utils/bcrypt.js
@@ -6,4 +6,8 @@ const generatePasswordHash = (password) => {
     return bcrypt.hash(password, SALT);
 };
 
-module.exports = { generatePasswordHash };
+const comparePassword = (password, passwordHash) => {
+    return bcrypt.compare(password, passwordHash);
+};
+
+module.exports = { generatePasswordHash, comparePassword };
