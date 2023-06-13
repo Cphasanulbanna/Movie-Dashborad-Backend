da033ccf650f04a3bbdfd8708a999fb48fffb9f1
HASANUL BANNA C P <cphasanulbanna@gmail.com>
2023-06-13 16:12:03 +0530

    genre api updated

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -27,8 +27,7 @@ const deleteGenre = async (req, res) => {
             return res.status(404).json({ message: "Genre id id required" });
         }
         const genre = await Genre.findByIdAndDelete(_id);
-        const genres = await Genre();
-        res.status(200).json({ message: `genre: ${genre} deleted successfully`, genres: genres });
+        res.status(200).json({ message: `genre: ${genre} deleted successfully` });
     } catch (error) {
         res.status(400).json({ message: error.message });
     }
