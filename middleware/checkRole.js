a551e21b7cd0e3d60e370e6f7eacdf8045740ba6
HASANUL BANNA C P <cphasanulbanna@gmail.com>
2023-06-17 18:41:05 +0530

    check role middleware added

---
+++ middleware/checkRole.js
@@ -0,0 +1,17 @@
+const User = require("../models/userModel");
+
+const checkRole = async (req, res, next) => {
+    try {
+        const user = await User.findById(req.userId);
+        if (user.role !== "Admin") {
+            return res
+                .status(401)
+                .json({ message: "Unauthorized , only admin can access these routes" });
+        }
+        next();
+    } catch (error) {
+        res.status(500).json({ message: "Internal server error" });
+    }
+};
+
+module.exports = { checkRole };
